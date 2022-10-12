b489f85ec7c6974d5231063ce680a1a082d584f4
Asif Ali <asif.a.7277@gmail.com>
2022-10-12 19:56:24 +0530

    Login lander page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import SignIn from "./SignIn";
 
 const Login = () => {
+  const [signIn, setsignIn] = useState(false);
   return (
     <div className="login">
       <div className="background">
@@ -11,7 +13,33 @@ const Login = () => {
            //assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
-        <button className="login_btn">Sign In </button>
+        <button className="login_btn" onClick={() => setsignIn(true)}>
+          Sign In{" "}
+        </button>
+        <div className="loginScreenGradient"></div>
+        {/* extra div is added here like a trick to provide the height of the above container  */}
+        <div className="loginScreenBody">
+          {signIn ? (
+            <SignIn />
+          ) : (
+            <>
+              <h1>Unlimited films,TV programmes and more.</h1>
+              <h2>Watch anywhere. Cancel anytime</h2>
+              <h3>
+                Ready to wantch ?Enter your email to create or restart your
+                membership
+              </h3>
+              <div className="loginScreenInput">
+                <form>
+                  <input type="email" placeholder="Email address" />
+                  <button className="inputBtn" onClick={() => setsignIn(true)}>
+                    Get Started
+                  </button>
+                </form>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
